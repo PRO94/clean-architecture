51cb03a161ed1c801330aca94e6def16ddbb3874
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-08-12 11:28:52 +0200

    Added JWT usage in API. Added all the configurations.

---
+++ src/Bookify/Bookify.Infrastructure/Authentication/AuthenticationOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace Bookify.Infrastructure.Authentication;
+
+public sealed class AuthenticationOptions
+{
+    public string Audience { get; init; } = string.Empty;
+
+    public string MetadataUrl { get; init; } = string.Empty;
+
+    public bool ReqireHttpsMetadata { get; init; }
+
+    public string Issuer { get; set; } = string.Empty;
+}
