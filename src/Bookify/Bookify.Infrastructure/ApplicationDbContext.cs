784729f4ddb31ad5d1dc69c58419a5590989ced9
Roman Prytulenko <roman.prytulenko@gmail.com>
2023-08-31 10:10:24 +0200

    Refactor ApplicationDbContext

--- src/Bookify/Bookify.Infrastructure/ApplicationDbContext.cs
+++ src/Bookify/Bookify.Infrastructure/ApplicationDbContext.cs
@@ -2,7 +2,6 @@
 using Bookify.Application.Exceptions;
 using Bookify.Domain.Abstractions;
 using Bookify.Infrastructure.Outbox;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
 
@@ -15,16 +14,13 @@ public sealed class ApplicationDbContext : DbContext, IUnitOfWork
         TypeNameHandling = TypeNameHandling.All
     };
 
-    private readonly IPublisher _publisher;
     private readonly IDateTimeProvider _dateTimeProvider;
 
     public ApplicationDbContext(
         DbContextOptions options, 
-        IPublisher publisher,
         IDateTimeProvider dateTimeProvider)
         : base(options)
     {
-        _publisher = publisher;
         _dateTimeProvider = dateTimeProvider;
     }
 
