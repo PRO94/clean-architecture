09dee08f5846f2fba63d82dcb6c919737a847c3e
Roman Prytulenko <roman.prytulenko@gmail.com>
2024-01-23 14:03:42 +0100

    Add permission-based authorization infrastructure and usage

--- src/Bookify/Bookify.Infrastructure/DependencyInjection.cs
+++ src/Bookify/Bookify.Infrastructure/DependencyInjection.cs
@@ -17,6 +17,7 @@ using Bookify.Infrastructure.Repositories;
 using Dapper;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -123,5 +124,9 @@ public static class DependencyInjection
         services.AddScoped<AuthorizationService>();
 
         services.AddTransient<IClaimsTransformation, CustomClaimsTransformation>();
+
+        services.AddTransient<IAuthorizationHandler, PermissionAuthorizationHandler>();
+
+        services.AddTransient<IAuthorizationPolicyProvider, PermissionAuthorizationPolicyProvider>();
     }
 }
